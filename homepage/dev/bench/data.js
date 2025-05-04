47e846a98124fe59b9d23cf392ccebe4f18ce24c
github-action-benchmark <github@users.noreply.github.com>
2025-05-04 22:39:16 +0000

    add Benchmark (pytest) benchmark result for 364b7e833c20d14e3416a335d0f9614b805a22b2

--- homepage/dev/bench/data.js
+++ homepage/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746397555671,
+  "lastUpdate": 1746398356252,
   "repoUrl": "https://github.com/uroni/hs5",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.2264400145604347",
             "extra": "mean: 15.270302101800098 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "martin@urbackup.org",
+            "name": "Martin Raiber",
+            "username": "uroni"
+          },
+          "committer": {
+            "email": "martin@urbackup.org",
+            "name": "Martin Raiber",
+            "username": "uroni"
+          },
+          "distinct": true,
+          "id": "364b7e833c20d14e3416a335d0f9614b805a22b2",
+          "message": "Try using manual vcpkg caching",
+          "timestamp": "2025-05-05T00:01:25+02:00",
+          "tree_id": "7a77875fa1a94763ce161713ac2e0f72a9cd2863",
+          "url": "https://github.com/uroni/hs5/commit/364b7e833c20d14e3416a335d0f9614b805a22b2"
+        },
+        "date": 1746398355268,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "test/test_perf.py::test_perf_upload_many_files_hs5",
+            "value": 0.07435389517137607,
+            "unit": "iter/sec",
+            "range": "stddev: 0.07085191981997827",
+            "extra": "mean: 13.449194526999964 sec\nrounds: 5"
+          },
+          {
+            "name": "test/test_perf.py::test_perf_upload_many_files_minio",
+            "value": 0.06628964366690474,
+            "unit": "iter/sec",
+            "range": "stddev: 0.18277268756793666",
+            "extra": "mean: 15.085312647400041 sec\nrounds: 5"
           }
         ]
       }
