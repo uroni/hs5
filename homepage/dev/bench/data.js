eb9e747ba5920d87598564b4952a43b353316d9a
github-action-benchmark <github@users.noreply.github.com>
2025-07-14 19:50:12 +0000

    add Benchmark (pytest) benchmark result for f7145583a1ee6452487b715d3fdbdd5bffbcae75

--- homepage/dev/bench/data.js
+++ homepage/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752522592971,
+  "lastUpdate": 1752522612127,
   "repoUrl": "https://github.com/uroni/hs5",
   "entries": {
     "Benchmark": [
@@ -1406,6 +1406,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.23476015789017873",
             "extra": "mean: 18.2197331856 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "martin@urbackup.org",
+            "name": "Martin Raiber",
+            "username": "uroni"
+          },
+          "committer": {
+            "email": "martin@urbackup.org",
+            "name": "Martin Raiber",
+            "username": "uroni"
+          },
+          "distinct": true,
+          "id": "f7145583a1ee6452487b715d3fdbdd5bffbcae75",
+          "message": "Fix default commit after ms",
+          "timestamp": "2025-07-14T21:41:18+02:00",
+          "tree_id": "cf4bdeffe19bd574bafd409b8fab32be77795f99",
+          "url": "https://github.com/uroni/hs5/commit/f7145583a1ee6452487b715d3fdbdd5bffbcae75"
+        },
+        "date": 1752522611033,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "test/test_perf.py::test_perf_upload_many_files_hs5",
+            "value": 0.07479899099189616,
+            "unit": "iter/sec",
+            "range": "stddev: 0.09315038001129212",
+            "extra": "mean: 13.369164299399994 sec\nrounds: 5"
+          },
+          {
+            "name": "test/test_perf.py::test_perf_upload_many_files_minio",
+            "value": 0.06647518426908115,
+            "unit": "iter/sec",
+            "range": "stddev: 0.173076081093226",
+            "extra": "mean: 15.043207642000004 sec\nrounds: 5"
           }
         ]
       }
